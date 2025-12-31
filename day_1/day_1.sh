1c63ccc488abee84f02779912cfcb7cd1d2bbe6d
Daksh Sawhney <darkstardaksh@gmail.com>
2025-12-31 14:13:14 +0530

    Day 1 done

---
+++ day_1/day_1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Log Generator
+# for i in {1..1000}; do
+#     echo "192.168.1.$((RANDOM % 20)) - - [$(date)] \"GET / HTTP/1.1\" $((RANDOM % 5 == 0 ? 500 : 200))" >> access.log
+# done
+
+LOG_FILE="$(dirname "$0")/access.log"
+
+awk '{ips[$1]++} END {for (ip in ips) print ips[ip], ip}' $LOG_FILE | sort -nr
