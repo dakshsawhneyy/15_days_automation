76578160c40ac5a7a67861c26cd563addc0689b6
Daksh Sawhney <darkstardaksh@gmail.com>
2026-01-05 23:18:43 +0530

    .split used as awk for differentiating logs

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -11,6 +11,13 @@ servers = [
     {"hostname": "cache-01", "cpu_usage": 99, "status": "active"}
 ]
 
+logs = [
+    "192.168.1.10 - - [10/Oct/2023] 'GET /home HTTP/1.1' 200",
+    "10.0.0.5 - - [10/Oct/2023] 'POST /api/login HTTP/1.1' 500",
+    "192.168.1.10 - - [10/Oct/2023] 'GET /about HTTP/1.1' 200",
+    "172.16.0.4 - - [10/Oct/2023] 'POST /api/pay HTTP/1.1' 500"
+]
+
 # try:
 #     with open(config_file, 'r') as f:
 #         content = f.read()
@@ -26,16 +33,22 @@ servers = [
 
 
 # Ticket 2
-print("----------------------------------------------------")
-print("printing servers with usage > 80 and are active are:")
-print("----------------------------------------------------")
-for server in servers:
-    usage = server["cpu_usage"]
-    status = server["status"]
+# print("----------------------------------------------------")
+# print("printing servers with usage > 80 and are active are:")
+# print("----------------------------------------------------")
+# for server in servers:
+#     usage = server["cpu_usage"]
+#     status = server["status"]
     
-    if usage > 80 and status == "active":
-        print(server["hostname"])
+#     if usage > 80 and status == "active":
+#         print(server["hostname"])
         
-# ANother writing technique
-problem_servers = [s["hostname"] for s in servers if s['cpu_usage'] > 80 and s['status'] == "active"]
-print(problem_servers)
+# # Another writing technique
+# problem_servers = [s["hostname"] for s in servers if s['cpu_usage'] > 80 and s['status'] == "active"]
+# print(problem_servers)
+
+# Ticket 3
+for log in logs:
+    status_code = log.split()[-1]   # -1 = $NF
+    if status_code == "500":
+        print(log.split()[0])
