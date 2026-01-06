65503dae405089d57a4e4090a07f7b0693941b63
Daksh Sawhney <darkstardaksh@gmail.com>
2026-01-06 17:40:52 +0530

    used post with requests to send payload

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -1,12 +1,30 @@
 import requests
 import json
+import uuid
 
-response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
+# Ticket 1:
+# response = requests.get("https://jsonplaceholder.typicode.com/posts/1")
 
-if response.status_code == 200:
-    print("API is Online")
-    json_res = response.json()
-    title = json_res["title"]
-    print("title: ", title)
+# if response.status_code == 200:
+#     print("API is Online")
+#     json_res = response.json()
+#     title = json_res["title"]
+#     print("title: ", title)
+# else:
+#     print("API is not Online")
+
+
+# Ticket 2:
+payload = {
+    "title": "Shubu saying hello",
+    "Age": "99",
+    "Lungi_Size": "XXXXXL"
+}
+
+response = requests.post("https://jsonplaceholder.typicode.com/posts", json=payload)
+print(response.json())
+
+if response.status_code == 201:
+    print(f"Alert Sent! Ticket ID: {uuid.uuid4()}")
 else:
-    print("API is not Online")
+    print("Error Occurred")
